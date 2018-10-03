e22aef90b0eb2ceabe70f9dbb9b3332ee907ecb0
RetoGrupo4 <43231873+RetoGrupo4@users.noreply.github.com>
2018-10-03 23:16:55 +0200

    Version 3.0

--- js/particulas.js
+++ js/particulas.js
@@ -6,17 +6,22 @@ $(document).ready(function(){
 
     particlesJS('particles-js', {
       particles: {
-        color: '#fff',
+        /*color: '#ff8',*/
+		color: '#fff',
         shape: 'circle', // "circle", "edge" or "triangle"
-        opacity: 1,
-        size: 4,
+        opacity: 0.8,
+        size: 3,
         size_random: true,
-        nb: 150,
+        nb: 75,
+		/*nb: 150,*/
         line_linked: {
           enable_auto: true,
-          distance: 100,
-          color: '#fff',
-          opacity: 1,
+          /*distance: 200,*/
+		  distance: 100,
+         /* color: '#fff',*/
+		  color: '#282828',
+		  
+          opacity: 0.1,
           width: 1,
           condensed_mode: {
             enable: false,
@@ -26,13 +31,13 @@ $(document).ready(function(){
         },
         anim: {
           enable: true,
-          speed: 2
+          speed: 1
         }
       },
       interactivity: {
         enable: true,
         mouse: {
-          distance: 300
+          distance: 500
         },
         detect_on: 'canvas', // "canvas" or "window"
         mode: 'grab',
@@ -53,3 +58,127 @@ $(document).ready(function(){
   }
   
 });
+
+/*particlesJS('particles-js',
+
+{
+"particles": {
+"number": {
+"value": 2000,
+"density": {
+"enable": true,
+"value_area": 8000
+}
+},
+"color": {
+"value": "#ffffff"
+},
+"shape": {
+"type": "circle",
+"stroke": {
+"width": 0,
+"color": "#000000"
+},
+"polygon": {
+"nb_sides": 5
+},
+"image": {
+"src": "img/github.svg",
+"width": 100,
+"height": 100
+}
+},
+"opacity": {
+"value": 0.5,
+"random": false,
+"anim": {
+"enable": false,
+"speed": 1,
+"opacity_min": 0.1,
+"sync": false
+}
+},
+"size": {
+"value": 5,
+"random": true,
+"anim": {
+"enable": false,
+"speed": 40,
+"size_min": 0.1,
+"sync": false
+}
+},
+"line_linked": {
+"enable": true,
+"distance": 150,
+"color": "#ffffff",
+"opacity": 0.4,
+"width": 1
+},
+"move": {
+"enable": true,
+"speed": 6,
+"direction": "none",
+"random": false,
+"straight": false,
+"out_mode": "out",
+"attract": {
+"enable": false,
+"rotateX": 600,
+"rotateY": 1200
+}
+}
+},
+"interactivity": {
+"detect_on": "canvas",
+"events": {
+"onhover": {
+"enable": true,
+"mode": "repulse"
+},
+"onclick": {
+"enable": true,
+"mode": "push"
+},
+"resize": true
+},
+"modes": {
+"grab": {
+"distance": 400,
+"line_linked": {
+"opacity": 1
+}
+},
+"bubble": {
+"distance": 400,
+"size": 40,
+"duration": 2,
+"opacity": 8,
+"speed": 3
+},
+"repulse": {
+"distance": 200
+},
+"push": {
+"particles_nb": 4
+},
+"remove": {
+"particles_nb": 2
+}
+}
+},
+"retina_detect": true,
+"config_demo": {
+"hide_card": false,
+"background_color": "#b61924",
+"background_image": "",
+"background_position": "50% 50%",
+"background_repeat": "no-repeat",
+"background_size": "cover"
+}
+}
+
+);
+
+
+*/
---
+++ js/section.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+
+         //script aside
+        var aside = getUrlParameter('aside');
+        if (aside == 0) {
+            $('#aside').append('<div include-html="aside/asideproxmox.html"></div>');
+        }
+        if (aside == 1) {
+            $('#aside').append('<div include-html="aside/asideubuntu.html"></div>');
+        }
+        if (aside == 2) {
+            $('#aside').append('<div include-html="aside/asidexamp.html"></div>');
+        }
+        if (aside == 3) {
+            $('#aside').append('<div include-html="aside/asidegit.html"></div>');
+        }
+        if (aside == 4) {
+            $('#aside').append('<div include-html="aside/asidesolucion.html"></div>');
+        }
+            //script section
+        var section = getUrlParameter('section');
+
+        if (section == 0) {
+
+            $('#section').append('<div include-html="section/UsbBooteable.html"></div>');
+        }
+        if (section == 1) {
+
+            $('#section').append('<div include-html="section/proxmox.html"></div>');
+        }
+        if (section == 2) {
+
+            $('#section').append('<div include-html="section/ubuntu.html"></div>');
+        }
+        if (section == 3) {
+
+            $('#section').append('<div include-html="section/xamp.html"></div>');
+        }
+        if (section == 4) {
+
+            $('#section').append('<div include-html="section/git.html"></div>');
+        }
+
+        if (section == 5) {
+
+            $('#section').append('<div include-html="section/versiones.html"></div>');
+        }
+        if (section == 6) {
+
+            $('#section').append('<div include-html="section/lenguajes.html"></div>');
+        }
+        if (section == 7) {
+
+$('#section').append('<div include-html="section/lenguajes.html"></div>');
+}
+
+        includeHTML();
