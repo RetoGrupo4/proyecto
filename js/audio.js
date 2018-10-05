c1d0687207338851092bae2140a8361a280980b8
RetoGrupo4 <43231873+RetoGrupo4@users.noreply.github.com>
2018-10-05 10:48:01 +0200

    Add files via upload

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,5 @@
 var audio=new Audio();//CREO LA VARIABLE AUDIO
-	audio.src="audio/r2d2.mp3"; //LE ASIGNO  EL SONIDO
+	audio.src="audio/r2d2.wav"; //LE ASIGNO  EL SONIDO
 	function play(){ // CREO LA FUNCION PARA QUE SE REPRODUCE EL AUDIO CON EL SONIDO QUE LE ASIGNE A LA VARIABLE AUDIO
 	audio.play();
 	}
